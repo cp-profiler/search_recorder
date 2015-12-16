84b9a2d56a41bba10f0f686c8ac67b7cb185f3cd
Chris Mears <chris@cmears.id.au>
2015-12-16 14:29:51 +1100

    Update for socket communication

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 OBJS = search_recorder.o message.pb.o
-CC = clang++
+CC = g++
 DEBUG = -g
 CFLAGS = -std=c++0x -W -Wall -c $(DEBUG)
-LFLAGS = -lzmq `pkg-config --cflags --libs protobuf`
+LFLAGS = `pkg-config --cflags --libs protobuf` -lboost_system
 
 search_recorder: $(OBJS)
 	$(CC) $(LFLAGS) $(OBJS) -o search_recorder
 search_recorder.o: search_recorder.cpp
 	$(CC) $(CFLAGS) search_recorder.cpp -o search_recorder.o
 message.pb.o: message.pb.cpp message.pb.hh
-	g++ -c message.pb.cpp -o message.pb.o
+	$(CC) $(CFLAGS) message.pb.cpp -o message.pb.o
 clean:
 	rm -f *.o search_recorder
--- search_recorder.cpp
+++ search_recorder.cpp
@@ -13,8 +13,15 @@
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/io/coded_stream.h>
 
-#include "third-party/zmq.hpp"
 #include "message.pb.hh"
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+
+using boost::asio::ip::tcp;
+using google::protobuf::io::OstreamOutputStream;
 
 bool writeDelimitedTo(
     const google::protobuf::MessageLite& message,
@@ -40,10 +47,94 @@ bool writeDelimitedTo(
   return true;
 }
 
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection> {
+public:
+    typedef boost::shared_ptr<tcp_connection> pointer;
 
+    static pointer create(OstreamOutputStream& raw_output, boost::asio::io_service& io_service) {
+        return pointer(new tcp_connection(raw_output, io_service));
+    }
+
+    tcp::socket& socket() {
+        return socket_;
+    }
+
+    void start() {
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(buffer, 4),
+                                boost::bind(&tcp_connection::handle_read_header, shared_from_this(),
+                                            boost::asio::placeholders::error));
+    }
+
+    void handle_read_header(const boost::system::error_code& error) {
+        if (error) {
+            std::cerr << "error reading header\n";
+            return;
+        }
+        uint32_t* int32p = reinterpret_cast<uint32_t*>(buffer);
+        len = *int32p;
+        if (len > 10000) abort();
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(buffer, len),
+                                boost::bind(&tcp_connection::handle_read_body, shared_from_this(),
+                                            boost::asio::placeholders::error));
+    }
+
+    void handle_read_body(const boost::system::error_code& error) {
+        if (error) return;
+        message::Node node;
+        node.ParseFromArray(buffer, len);
+        writeDelimitedTo(node, &raw_output_);
+        if (node.type() == message::Node::DONE)
+            socket_.get_io_service().stop();
+        else
+            start();
+    }
+
+private:
+    tcp_connection(OstreamOutputStream& raw_output, boost::asio::io_service& io_service)
+        : socket_(io_service), raw_output_(raw_output)
+    { }
+
+    tcp::socket socket_;
+    char buffer[10000];
+    int len;
+    OstreamOutputStream& raw_output_;
+};
+
+
+class tcp_server {
+public:
+    tcp_server(OstreamOutputStream& raw_output, boost::asio::io_service& io_service)
+        : acceptor_(io_service, tcp::endpoint(tcp::v4(), 6565)),
+          raw_output_(raw_output) {
+        start_accept();
+    }
+
+private:
+    void start_accept() {
+        tcp_connection::pointer new_connection =
+            tcp_connection::create(raw_output_, acceptor_.get_io_service());
+
+        acceptor_.async_accept(new_connection->socket(),
+                               boost::bind(&tcp_server::handle_accept, this,
+                                           new_connection,
+                                           boost::asio::placeholders::error));
+    }
+
+    void handle_accept(tcp_connection::pointer new_connection,
+                       const boost::system::error_code& error) {
+        if (!error) new_connection->start();
+        start_accept();
+    }
+
+    tcp::acceptor acceptor_;
+    OstreamOutputStream& raw_output_;
+};
+
+
+        
 int main(int argc, char** argv) {
-
-  using google::protobuf::io::OstreamOutputStream;
 
   std::string path = "data.db";
 
@@ -60,31 +151,13 @@ int main(int argc, char** argv) {
   std::ofstream outputFile(path, std::ios::out | std::ios::binary);
   OstreamOutputStream raw_output(&outputFile);
 
-  zmq::context_t context(1);
-  zmq::socket_t receiver (context, ZMQ_PULL);
-
-  receiver.bind("tcp://*:6565");
-  std::cout << "listening to port: 6565\n";
-
-    if (!outputFile) {
-      std::cerr << "Can't open the file" << std::endl;
-      return 1;
-    }
-
-    while (true) {
-      
-      zmq::message_t msg;
-      
-      receiver.recv(&msg);
-
-      message::Node node;
-      node.ParseFromArray(msg.data(), msg.size());
-      writeDelimitedTo(node, &raw_output);
-
-      if (node.type() == message::Node::DONE)
-        break;
-
-    }
+  try {
+      boost::asio::io_service io_service;
+      tcp_server server(raw_output, io_service);
+      io_service.run();
+  } catch (std::exception& e) {
+      std::cerr << e.what() << "\n";
+  }
 
   return 0;
 }
